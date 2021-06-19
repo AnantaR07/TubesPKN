f328af45272b918ff2d1258a9401fdf0de4ce436
IbrahimgilangR <80778648+IbrahimgilangR@users.noreply.github.com>
2021-06-19 15:28:59 +0700

    Update loginControllerdosen.java

--- mainclass/loginControllerdosen.java
+++ mainclass/loginControllerdosen.java
@@ -28,6 +28,37 @@ public class loginControllerdosen{
     private Scene scene;
     private Parent root;
     
+    public TextField username;
+    public TextField password;
+        
+    public void login(ActionEvent actionevent) throws IOException, SQLException {
+    databasedosen dbdosen = new databasedosen();
+        Connection connection = databasedosen.getConnection();
+        String query = "SELECT * FROM user where Username=? and Password=?";
+        PreparedStatement pst = connection.prepareStatement(query);
+        pst.setString(1, username.getText());
+        pst.setString(2, password.getText());
+        ResultSet rs = pst.executeQuery();
+
+        if(rs.next()){
+            JOptionPane.showMessageDialog(null, "username dan Password benar");
+            Parent root = FXMLLoader.load(getClass().getResource(".fxml"));
+            stage = (Stage) ((Node) actionevent.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }else {
+            username.setText("");
+            password.setText("");
+            JOptionPane.showMessageDialog(null, "username dan Password salah");
+            Parent root = FXMLLoader.load(getClass().getResource("FXMLlogindosen.fxml"));
+            stage = (Stage) ((Node) actionevent.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+    
      public void back(ActionEvent actionevent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLlogin.fxml"));
         stage = (Stage) ((Node) actionevent.getSource()).getScene().getWindow();
