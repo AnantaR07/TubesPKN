708c2916b2bae466afa4f720da4430908bbf3902
IbrahimgilangR <80778648+IbrahimgilangR@users.noreply.github.com>
2021-06-19 15:12:30 +0700

    Update loginControllermahasiswa.java

--- mainclass/loginControllermahasiswa.java
+++ mainclass/loginControllermahasiswa.java
@@ -32,41 +32,29 @@ public class loginControllermahasiswa {
     private Scene scene;
     private Parent root;
 
-    public ObservableList<setgetdata> getDataList() throws SQLException {
-        ObservableList<setgetdata> dataList = FXCollections.observableArrayList();
-        databasemahasiswa dbmahasiswa = new databasemahasiswa();
-        Connection connection = databasemahasiswa.getConnection();
-        String query = "SELECT * FROM user";
-        Statement st;
-        ResultSet rs;
-
-        try {
-            st = connection.createStatement();
-            rs = st.executeQuery(query);
-            setgetdata data;
-            while (rs.next()) {
-                data = new setgetdata(rs.getString("Username"), rs.getString("Password"));
-                dataList.add(data);
-            }
-        } catch (Exception e) {
-            System.out.println("Error : " + e.getMessage());
-        }
-        return dataList;
-    }
-
     public TextField username;
     public TextField password;
 
-    public void login(ActionEvent actionevent) throws IOException, SQLException {
-        ObservableList<setgetdata> list = getDataList();
-       
-        if (username.equals(getString("Username")) ) {
-            Parent root = FXMLLoader.load(getClass().getResource("FXMLlogin.fxml"));
+  public void login(ActionEvent actionevent) throws IOException, SQLException {
+        databasemahasiswa dbmahasiswa = new databasemahasiswa();
+        Connection connection = databasemahasiswa.getConnection();
+        String query = "SELECT * FROM user where Username=? and Password=?";
+        PreparedStatement pst = connection.prepareStatement(query);
+        pst.setString(1, username.getText());
+        pst.setString(2, password.getText());
+        ResultSet rs = pst.executeQuery();
+
+        if(rs.next()){
+            JOptionPane.showMessageDialog(null, "username dan Password benar");
+            Parent root = FXMLLoader.load(getClass().getResource(".fxml"));
             stage = (Stage) ((Node) actionevent.getSource()).getScene().getWindow();
             scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
         }else {
+            username.setText("");
+            password.setText("");
+            JOptionPane.showMessageDialog(null, "username dan Password salah");
             Parent root = FXMLLoader.load(getClass().getResource("FXMLloginmahasiswa.fxml"));
             stage = (Stage) ((Node) actionevent.getSource()).getScene().getWindow();
             scene = new Scene(root);
