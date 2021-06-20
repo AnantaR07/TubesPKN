b85f283c30685d3db95582c2879a38010cf43c9b
Ananta Romadhan <80764638+AnantaR07@users.noreply.github.com>
2021-06-20 17:52:15 +0700

    Add files via upload

---
+++ conecctor/databasedosen.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conecctor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author USER
+ */
+public class databasedosen {
+    public static Connection connection;
+    public static Connection getConnection() throws SQLException{
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_dosen","root","");
+            
+            
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
