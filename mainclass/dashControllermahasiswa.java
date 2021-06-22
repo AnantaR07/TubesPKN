9dd10696dfd6a1868704bc62b932134dfcc127de
Ananta Romadhan <80764638+AnantaR07@users.noreply.github.com>
2021-06-22 23:28:24 +0700

    Add files via upload

---
+++ mainclass/dashControllermahasiswa.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainclass;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author USER
+ */
+public class dashControllermahasiswa {
+
+    @FXML
+    private RadioButton fxmahasiswa, fxdosen, fxtu;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    public void pengajuanproposal(ActionEvent actionevent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLuploadmahasiswa.fxml"));
+        stage = (Stage) ((Node) actionevent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+        public void pendaftaranujian(ActionEvent actionevent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(".fxml"));
+        stage = (Stage) ((Node) actionevent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
+    }
+
+}
